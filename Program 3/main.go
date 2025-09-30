35e23b9b8bab766453f6ba6b211f7ed02b918389
default <zoe.wrubleski@stu.ocsb.ca>
2025-09-30 14:04:37 +0000

    volume of a sphere with a radius of 4cm calculated using ts

--- Program 3/main.go
+++ Program 3/main.go
@@ -2,5 +2,15 @@
  *@author Zoe Wrubleski
  *@version 1.0.0
  *@date 2025-09-30
- *@fileoverview 
+ *@fileoverview This program calculates the volume of a sphere with a radius of 4 cm
  */
+
+package main
+
+import "fmt"
+
+func main() {
+	//
+	fmt.Println("V ≈ " + "cm³")
+
+}
--- Program 3/main.ts
+++ Program 3/main.ts
@@ -2,5 +2,13 @@
  * @author Zoe Wrubleski
  * @version 1.0.0
  * @date 2025-09-30
- * @fileoverview 
+ * @fileoverview This program calculates the volume of a sphere with a radius of 4 cm
  */
+
+//text asking the question
+console.log("What is the volume of a sphere with a radius of 4 cm?");
+
+//calculation
+console.log("V ≈ " + (4 / 3 * 3.141592653589 * 4 * 4 * 4) + " cm³");
+
+console.log("\nDone.");
