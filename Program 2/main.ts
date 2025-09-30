240c42e435cf6c5152b0e096088857f23d129c84
default <zoe.wrubleski@stu.ocsb.ca>
2025-09-30 13:51:14 +0000

    using ts to calculate the area of a circle with a radius of 14cm

--- Program 2/main.ts
+++ Program 2/main.ts
@@ -5,3 +5,10 @@
  * @fileoverview This program calculates the area of a circle with a radius of 14 cm
  */
 
+//text giving the question
+console.log("If a circle has a radius of 14, what is the area?");
+
+//calculation
+console.log("A ≈ " + (3.141592653589 * 14 * 14) + " cm²");
+
+console.log("\nDone.");
