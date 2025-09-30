19d268c269439add7cb2d7d502357bfbc0ad7b51
default <zoe.wrubleski@stu.ocsb.ca>
2025-09-30 13:45:37 +0000

    using go to calculate the area of a circle with a 14cm radius

--- Program 2/main.go
+++ Program 2/main.go
@@ -2,5 +2,18 @@
  *@author Zoe Wrubleski
  *@version 1.0.0
  *@date 2025-09-30
- *@fileoverview 
+ *@fileoverview This program calculates the area of a circle with a radius of 14 cm
  */
+
+package main
+
+import "fmt"
+
+func main() {
+	//
+	fmt.Println("If a circle has a radius of 14, what is the area?")
+
+	//
+	fmt.Println("A ≈ " + fmt.Sprint(3.141592653589 * 14 * 14) + " cm²")
+
+}
--- Program 2/main.ts
+++ Program 2/main.ts
@@ -2,5 +2,6 @@
  * @author Zoe Wrubleski
  * @version 1.0.0
  * @date 2025-09-30
- * @fileoverview 
+ * @fileoverview This program calculates the area of a circle with a radius of 14 cm
  */
+
