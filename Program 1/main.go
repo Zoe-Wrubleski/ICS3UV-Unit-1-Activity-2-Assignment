e4a8f00102fbbe5d09bd6a56fdcc5d2a47919bfb
default <zoe.wrubleski@stu.ocsb.ca>
2025-09-30 13:33:06 +0000

    using go to convert 34 degrees C to F

--- Program 1/main.go
+++ Program 1/main.go
@@ -2,5 +2,19 @@
  *@author Zoe Wrubleski
  *@version 1.0.0
  *@date 2025-09-30
- *@fileoverview 
+ *@fileoverview This program converts 34 degrees Celsius to its Fahrenheit equivalent
  */
+
+package main
+
+import "fmt"
+
+func main() {
+	//text explaining calculation
+	fmt.Println("What is 34 degrees Celsius in Fahrenheit?")
+
+	//conversion of 34 degrees C to F
+	fmt.Println("34 C = " + fmt.Sprint(9 / 5 * 34 + 32) + " F")
+
+	fmt.Println("\nDone.")
+}
