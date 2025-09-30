4ea83bb67730936b59b2520c6cd34f31b326df64
default <zoe.wrubleski@stu.ocsb.ca>
2025-09-30 14:18:08 +0000

    using ts to calculate the gross pay of Fred

--- Program 4/main.go
+++ Program 4/main.go
@@ -2,5 +2,20 @@
  *@author Zoe Wrubleski
  *@version 1.0.0
  *@date 2025-09-30
- *@fileoverview 
+ *@fileoverview This program calculates the gross pay for an employee named Fred, who worked 40 hours at an hourly wage of $8.20
  */
+
+package main
+
+import "fmt"
+
+func main() {
+	//text asking the question
+	fmt.Println("If Fred worked for 40 hours at an hourly wage of $8.20, what is his gross pay?")
+
+	//calculation
+	fmt.Println("Gross pay = $" + fmt.Sprint(40 * 8.20))
+
+	fmt.Println("\nDone.")
+
+}
--- Program 4/main.ts
+++ Program 4/main.ts
@@ -2,5 +2,13 @@
  * @author Zoe Wrubleski
  * @version 1.0.0
  * @date 2025-09-30
- * @fileoverview 
+ * @fileoverview This program calculates the gross pay for an employee named Fred, who worked 40 hours at an hourly wage of $8.20
  */
+
+//text asking the question
+console.log("If Fred worked for 40 hours at an hourly wage of $8.20, what is his gross pay?");
+
+//calculation
+console.log("Gross pay = $" + (40 * 8.20));
+
+console.log("\nDone.");
