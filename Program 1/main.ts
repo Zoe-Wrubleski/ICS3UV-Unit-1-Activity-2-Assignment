e81e5759186cabd76868304f84867a623c6a81ee
default <zoe.wrubleski@stu.ocsb.ca>
2025-09-30 13:26:59 +0000

    ts code to convert 34 degrees C to F

---
+++ Program 1/main.ts
@@ -0,0 +1,14 @@
+/**
+ * @author Zoe Wrubleski
+ * @version 1.0.0
+ * @date 2025-09-30
+ * @fileoverview This program converts a temperature of 34 degrees Celsius to its Fahrenheit equivalent
+ */
+
+//text explaining the calculation
+console.log("What is 34 degrees Celsius in Fahrenheit?");
+
+//conversion of 34 C to F
+console.log("34 C = " + (9 / 5 * 34 + 32) + (" F"));
+
+console.log("\nDone.");
---
+++ Program 4/main.ts
@@ -0,0 +1,6 @@
+/**
+ * @author Zoe Wrubleski
+ * @version 1.0.0
+ * @date 2025-09-30
+ * @fileoverview 
+ */
